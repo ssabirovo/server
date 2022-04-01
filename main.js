a7df977fdef2bd2aa7110f83dc31232209ad82a7
Arslonbek Alimbayev <arslonbekalimbayev@Arslonbeks-MacBook-Pro.local>
2022-04-01 11:38:42 +0500

    lesson-48 finish-code

--- main.js
+++ main.js
@@ -1,41 +1,58 @@
-// ES5
-// var age = 20; // number
+// let numbers = [1, 2, 3];
+// console.log("type = ", typeof []);
+// console.log("type = ", typeof false);
+// console.log("type = ", typeof null);
+// console.log("type = ", typeof 123);
+// console.log("type = ", typeof "123");
 
-// ES6
-// primitive type
-// let a = 20;
-// let b = a;
-// a = 50;
-// console.log(`a = ${a}`);
-// console.log(`b = ${b}`);
+// function addTwoNumber(a, b) {
+//   console.log("a + b = " + (a + b));
+// }
 
-// reference type
-// let person1 = {
-//   age: 20,
-//   job: "Teacher",
+// let x = 20;
+// let y = 60;
+// let z = "20";
+// let c = z + x - y; // NAN -> not a number
+// console.log("c = ", typeof NAN);
+
+// addTwoNumber(20, 15);
+// let x = 21; //  number
+// let y = 4;
+// // x = x + 20;
+// // x %= 20;
+// console.log("x = ", x);
+// // console.log("x = ", ++x);
+// // console.log("y = ", y);
+// Comparison  operators
+// let x = 30;
+// let y = 30;
+// let z = x <= y;
+// console.log("z = ", z);
+
+// #7 -> Equality operators
+// let x = 1;
+// let y = 0;
+
+// let z = x === y;
+// console.log("z = ", z);
+
+// let b = 20; // string
+// console.log("typeof b = ", typeof typeof b);
+
+// #8 -> Logical operators
+// let users = {
+//   "001": {},
+//   "002": {},
 // };
-// let person2 = person1;
-// person1.job = "Developer";
-// person2.age = 60;
-// console.log("person1 = ", person1);
+// let isExistThirdUser = !!users["003"];
+// if (isExistThirdUser) {
+//   alert("Hello third user");
+// }
 
-// String firstName = 50;
+// console.log("isExistThirdUser = ", isExistThirdUser);
 
-// let numbers1 = [1, 2, 3];
-// let numbers2 = numbers1;
-// numbers2[0] = null;
+// #10 -> String and template literals
 
-// console.log("numbers1 = ", numbers1);
-
-// console.log("numbers2 = ", numbers2);
-
-// Object type
-const person = {
-  age: 20,
-  job: "teacher",
-};
-// dot notation
-console.log("job = ", person.job);
-
-// bracket notation
-console.log("job = ", person["job"]);
+// const x = "arslonbek";
+// console.log("x = " + x);
+// console.log(`x = ${}` + x);
