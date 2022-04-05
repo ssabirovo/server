6f759342bc2b9cc16be6cafef33977a73d1184f7
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-05 06:01:35 +0500

    lesson-50 start-code

---
+++ scripts/strict.js
@@ -0,0 +1,27 @@
+// function first() {
+//   "use strict";
+//   age = 20;
+//   console.log("age = ", age);
+// }
+// function second() {
+//   age = 20;
+//   console.log("age = ", age);
+// }
+
+// first();
+
+// const obj = {
+//   firstName: "arslonbek",
+//   get firstName() {
+//     return this.firstName;
+//   },
+//   set firstName(newName) {
+//     this.firstName = newName;
+//   },
+// };
+
+// obj.firstName = "arslonbek";
+// obj.age = 20;
+// obj.count = 20;
+
+// console.log("obj = ", obj);
