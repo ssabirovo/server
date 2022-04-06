ff5e9d7b426ffa1721e8ca4c9e99fa50d39bc30b
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-06 13:12:25 +0500

    lesson-51 finish-code

--- main.js
+++ main.js
@@ -70,3 +70,70 @@
 
 // const data = show(3, 4, 1 / 2);
 // console.log("data = ", data);
+
+// Array
+// let jora1 = "Arslonbek";
+// let jora2 = "Bobur";
+// let jora3 = "Erkin";
+// let jora4 = "Islom";
+// let jora5 = "Siroj";
+// let jora6 = "Toxir";
+//                0           1        2        3         4       5 ...
+// const joralar = ["Arslonbek", "Bobur", "Erkin", "Islom", "Siroj", "Toxir"];
+
+// // jora1 -> joralar[0]
+// // jora2 -> joralar[1]
+
+// console.log("joralar = ", joralar);
+
+// joralar[1] = "Boburbek";
+
+// console.log("joralar = ", joralar);
+
+// joralar.splice(1, 1);
+// console.log("joralar = ", joralar);
+
+// const years = Array(20);
+// console.log("years[20] = ", years[15]);
+
+// const list = [true, "arslonbek", null];
+// list[3] = null;
+// list[20] = "jamshid";
+// console.log("list = ", list);
+// console.log("list.length = ", typeof list);
+
+// console.log("Hello World 1");
+// console.log("Hello World 2");
+// console.log("Hello World 3");
+// console.log("Hello World 4");
+// console.log("Hello World 5");
+// console.log("Hello World 6");
+// console.log("Hello World 7");
+
+// for (let i = 20; i >= 0; i--) {
+//   console.log(` i = ${i}`);
+// }
+
+/**
+ * cases
+ *
+ * 1 : a = 20; b = 15
+ * 2 : a = -10; b =0
+ * 3 : a = 10; b =5.6
+ * 4: a = 6; b = 7;
+ *
+ *
+ *
+ */
+// function findFive(a, b) {
+//   // for (let i = 1; true; i++) {
+//   // for (let j = 1; j < i; j++) {
+//   // }
+//   // }
+// }
+
+// findFive(10);
+//               012345678
+let firstName = "arslonbek";
+console.log("firstName[2] = ", firstName[2]);
+console.log("firstName.length = ", firstName.length);
