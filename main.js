85209901a017fe16ee36baf709b504f2373f679a
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-06 10:56:42 +0500

    finish function section

--- main.js
+++ main.js
@@ -1,3 +1,72 @@
 "use strict";
 
+// function logger(name, lastName, age) {
+//   // function body
+//   console.log(name + " " + lastName + " " + age + " yoshda");
+//   return name;
+// }
 
+// let firstName = logger("Jamshidbek", "Alimbayev", 21);
+// console.log("firstName = ", firstName);
+
+// let a = 10;
+// a = myPow(a);
+// console.log("a = ", a);
+
+// function myPow(a) {
+//   return a * a;
+// }
+
+// function logSum(x, y, z) {
+//   console.log(`${x} + ${y} + ${z} = `, x + y + z);
+// }
+
+// logSum(1, 2, 3);
+
+/**
+ *
+ * width : number
+ * height : number
+ * return: number
+ */
+// function areaSum(width, height) {
+//   return width * height;
+// }
+/**
+ * bizda ikkita togri torutburchak
+ * birinchi -> w: 20; h: 30;
+ * ikkinchi -> w: 30; h: 15;
+ *
+ */
+
+// let width1 = 20;
+// let height1 = 30;
+// let width2 = 30;
+// let height2 = 15;
+// let area1 = areaSum(width1, height1);
+// let area2 = areaSum(width2, height2);
+// console.log("area1 = ", area1);
+// console.log("area2 = ", area2);
+
+// let a = 20;
+// let b = a;
+// a = 30;
+// console.log(`a = ${a}`);
+// console.log(`b = ${b}`);
+
+//  {} -> block scope
+// function  {} -> function scope
+// calling, running, invoke function
+
+// (a**2+b**2)**(1/pow)
+// function show(a, b, pow) {
+//   const amount = a * a + b * b;
+//   return customPow(amount, pow);
+// }
+
+// function customPow(amount, pow) {
+//   return amount ** (1 / (1 / 2));
+// }
+
+// const data = show(3, 4, 1 / 2);
+// console.log("data = ", data);
