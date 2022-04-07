ad5a79575b7f79fc089facf9ea6547a7fad28e1d
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-07 16:55:56 +0500

    lesson-52 start-code

--- main.js
+++ main.js
@@ -1,42 +1,77 @@
 "use strict";
 
-//          0123456789     2345678
-// let text = "helloworld"; // lloworl
+// //          0123456789     2345678
+// // let text = "helloworld"; // lloworl
 
-// let tempText = "";
-// for (let i = 2; i < text.length - 1; i++) {
-//   tempText = tempText + text[i];
-//   console.log(`i = ${i} -> tempText = ${tempText}`);
+// // let tempText = "";
+// // for (let i = 2; i < text.length - 1; i++) {
+// //   tempText = tempText + text[i];
+// //   console.log(`i = ${i} -> tempText = ${tempText}`);
+// // }
+// // console.log("tempText = ", tempText);
+
+// // Daftar
+// /**
+//  *  i = 2 -> tempText = l
+//  *  i = 3 -> tempText = ll
+//  *  i = 4 -> tempText = llo
+//  *  i = 5 -> tempText = llow
+//  *  i = 6 -> tempText = llowo
+//  *  i = 7 -> tempText = llowor
+//  *  i = 8 -> tempText = lloworl
+//  *
+//  *
+//  *
+//  */
+
+// function subString(text, startIndex, endIndex) {
+//   if (endIndex > text.length) endIndex = text.length;
+
+//   let tempText = "";
+//   for (let i = startIndex; i < endIndex; i++) {
+//     tempText = tempText + text[i]; //
+//     // tempText = "" + "c" -> "c"
+//   }
+
+//   return tempText;
 // }
-// console.log("tempText = ", tempText);
 
-// Daftar
-/**
- *  i = 2 -> tempText = l
- *  i = 3 -> tempText = ll
- *  i = 4 -> tempText = llo
- *  i = 5 -> tempText = llow
- *  i = 6 -> tempText = llowo
- *  i = 7 -> tempText = llowor
- *  i = 8 -> tempText = lloworl
- *
- *
- *
- */
+// /**
+//  *  Loop
+//  *
+//  * i = 2 -> tempText = "" + "c" -> "c"
+//  * i = 3 -> tempText = "c" + "d" -> "cd"
+//  * i = 4 -> tempText = "cd" + "e" -> "cde"
+//  * i = 5 -> tempText = "cde" + "f" -> "cdef"
+//  * i = 6 -> stop;
+//  *
+//  */
 
-function subString(text, startIndex, endIndex) {
-  let tempText = "";
-  for (let i = startIndex; i < endIndex; i++) {
-    console.log(`text[${i}] = ${text[i]}`);
-    tempText = tempText + text[i];
-  }
-}
+// //         01234
+// subString("hello", 1, 4); // ell
 
-//         01234
-subString("hello", 1, 4); // ell
+// //         012345
+// const data = subString("abcdef", 1, 20); //  bcdef
+// console.log("data = ", data);
 
-//         012345
-// subString("abcabc", 3, 4); //  a
-
-// //         0
+// // //         0
 // subString("a", 0, 0); //  ""
+const users = [
+  "Hasanboy",
+  "Muhammaddiyor Q",
+  "Abdunosir",
+  "Muhammaddiyor R",
+  "Inomjon",
+  "Rahmon",
+  "Muhiddin",
+  "Husniddin",
+  "Abdulholiq",
+  "Muhammadali",
+  "Muhammadumar",
+  "Avazbek",
+  "Samandar",
+  "Murtozxon",
+  "Dilshodbek",
+].sort(() => Math.random() - 0.5);
+
+console.log("users = ", users);
