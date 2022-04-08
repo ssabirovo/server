16c7020dd9eec769f7230c624424c97b203a8db8
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-08 17:05:36 +0500

    lesson-54 start-code

--- main.js
+++ main.js
@@ -1,12 +1,62 @@
-function delDel(str) {
-  let tempText = "";
-  for (let i = 0; i < str.length; i++) {
-    if (str[1] === "d" && str[2] === "e" && str[3] === "l") {
-      continue;
-    } else {
-      tempText += str[i];
-    }
-  }
+/* 20 String Methods */
 
-  return tempText;
-}
+var stringOne = "PDP is the best place to learn";
+var stringTwo = "frontend and backend development";
+
+// charAt()
+console.log(stringOne.charAt(1));
+
+// charCodeAt()
+// console.log(stringOne.charCodeAt(1));
+
+// concat()
+// console.log(stringOne.concat(stringTwo));
+
+// endsWith()
+// console.log(stringOne.endsWith("to"));
+
+// includes()
+// console.log(stringTwo.includes("end"));
+
+// indexOf()
+// console.log(stringTwo.indexOf("end"));
+
+// lastIndexOf()
+// console.log(stringTwo.lastIndexOf("end"));
+
+// match()
+// console.log(stringTwo.match(/end/g));
+
+// repeat()
+// console.log(stringOne.repeat(3));
+
+// replace()
+// console.log(stringTwo.replace(/end/g, "END"));
+
+// search()
+// console.log(stringTwo.search("end"));
+
+// slice()
+// console.log(stringTwo.slice(2, 4));
+
+// split()
+// console.log(stringOne.split(" "));
+
+// startsWith()
+// console.log(stringOne.startsWith("free"));
+
+// substr()
+// console.log(stringTwo.substr(2, 4));
+
+// substring()
+// console.log(stringTwo.substring(2, 4));
+
+// toLowerCase()
+// console.log(stringOne.toLowerCase());
+
+// toUpperCase()
+// console.log(stringOne.toUpperCase());
+
+// trim()
+var stringThree = "     Subscribe now!      ";
+// console.log(stringThree.trim());
