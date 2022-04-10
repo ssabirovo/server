61346f993d79ae011650225abb773cf4999d0ca1
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-11 01:02:02 +0500

    lesson-55 start-code

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
-/* 20 String Methods */
-
-var stringOne = "PDP is the best place to learn";
-var stringTwo = "frontend and backend development";
-
-// charAt()
-console.log(stringOne.charAt(1));
-
-// charCodeAt()
-// console.log(stringOne.charCodeAt(1));
-
-// concat()
-// console.log(stringOne.concat(stringTwo));
-
-// endsWith()
-// console.log(stringOne.endsWith("to"));
-
-// includes()
-// console.log(stringTwo.includes("end"));
-
-// indexOf()
-// console.log(stringTwo.indexOf("end"));
-
-// lastIndexOf()
-// console.log(stringTwo.lastIndexOf("end"));
-
-// match()
-// console.log(stringTwo.match(/end/g));
+let stringOne = "PDP is the best to learn";
+let stringTwo = "FrontEnd and Backend development";
 
 // repeat()
 // console.log(stringOne.repeat(3));
@@ -41,22 +15,3 @@ console.log(stringOne.charAt(1));
 
 // split()
 // console.log(stringOne.split(" "));
-
-// startsWith()
-// console.log(stringOne.startsWith("free"));
-
-// substr()
-// console.log(stringTwo.substr(2, 4));
-
-// substring()
-// console.log(stringTwo.substring(2, 4));
-
-// toLowerCase()
-// console.log(stringOne.toLowerCase());
-
-// toUpperCase()
-// console.log(stringOne.toUpperCase());
-
-// trim()
-var stringThree = "     Subscribe now!      ";
-// console.log(stringThree.trim());
