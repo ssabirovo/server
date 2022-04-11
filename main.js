e3fdebf6800e5d54334565d5d6e0172ac2e3e5fc
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-11 12:53:58 +0500

    lesson-55 finish-code

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
-let stringOne = "PDP is the best to learn";
-let stringTwo = "FrontEnd and Backend development";
+let stringOne = "PDP is the best is to learn";
+let stringTwo = "Frontend  and   Backend development end";
 
 // repeat()
-// console.log(stringOne.repeat(3));
+// console.log("arslonbek".repeat(3));
 
-// replace()
-// console.log(stringTwo.replace(/end/g, "END"));
+// regex
+// stringTwo = stringTwo.replace(/y[a-zA-Z]k/gi, "hello");
+// console.log(stringTwo);
 
 // search()
-// console.log(stringTwo.search("end"));
+// console.log(stringTwo.search(/en[a-zA-Z]/i));
 
 // slice()
 // console.log(stringTwo.slice(2, 4));
+// console.log(stringTwo);
 
 // split()
-// console.log(stringOne.split(" "));
+// console.log(stringOne.split(/is/));
+
+// function repeat(str, amount) {
+//   let tempText = "";
+//   for (let i = 0; i < amount; i++) {
+//     tempText += str;
+//   }
+//   return tempText;
+// }
+
+// console.log("ars".repeat(4));
+
+// console.log(repeat("ars", 4));
+
+function includes(fullText = "", searchText = "") {
+  let isExist = false;
+  for (let i = 0; i < fullText.length; i++) {
+    if (fullText.substring(i, searchText.length + i) === searchText) {
+      isExist = true;
+      break;
+    }
+  }
+  return isExist;
+}
+
+let text = "jamshidbek mani urdi.";
+
+console.log(text.includes("man"));
+console.log(includes(text, "man"));
