cdbdadc5789ae221357f0236982c59e711e16be0
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-19 09:16:10 +0500

    solutionWithoutSet

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-const items = ["🍅", "🍄", "🥦", "🥒", "🌽", "🥕", "🥑"];
+const items = ["🍅", "🍄", "🥦", "🥕", "🥦", "🥒", "🌽", "🥕", "🥑"];
+
+// const isArray = Array.isArray(items);
+// console.log("isArray = ", isArray);
+// console.log(items);
+// let mySet = new Set(items);
+
+// items.forEach(function (a) {
+//   console.log(a);
+// });
+// for (let i = 0; i < items.length; i++) {
+//   console.log(items[i]);
+// }
+// console.log("-------------");
+// mySet.forEach(function (a) {
+//  console.log(a);
+// });
+// for (let i = 0; i < mySet.size; i++) {
+//   console.log(mySet[i]);
+// }
+
+// solution without Set
+function solutionWithoutSet(list = []) {
+  const itemsObj = {};
+  for (let i = 0; i < items.length; i++) {
+    let _key = items[i];
+    itemsObj[_key] = _key;
+  }
+  const newList = Object.values(itemsObj);
+  console.log(list);
+  console.log(newList);
+}
+
+// const person = {
+//   age: 20,
+//   phone: "+998...",
+// };
+// console.log("person = ", person);
+
+solutionWithoutSet(items);
