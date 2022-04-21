8a2b280bc684712a6a2ba23e982d286ba1b1e478
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-21 12:46:23 +0500

    lesson-62 finish-code

--- filter.js
+++ filter.js
@@ -1,16 +1,16 @@
-const numbers = [1, 15, 13, 12, 22, 45, 36, 30, 10, 20];
-
 /**
  *
  * @param {number[]} nums
  * @returns {
-    fizz: [12, 36],
-    buzz: [10, 20],
-    fizzBuzz: [15, 45, 30],
-    others: [1, 13, 22],
-  };
- */
-function fizzBuzz(nums = []) {
+ fizz: [12, 36],
+ buzz: [10, 20],
+ fizzBuzz: [15, 45, 30],
+ others: [1, 13, 22],
+};
+*/
+
+const numbers = [1, 15, 13, 12, 22, 45, 36, 30, 10, 20];
+function fizzBuzz(nums) {
   const fizz = nums.filter(function (num) {
     return num % 3 === 0 && num % 5 !== 0;
   });
