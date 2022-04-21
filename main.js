4da6c549fdda415be1c9f33f6e41022f6f9597d2
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-21 09:59:08 +0500

    add new codes for custom-problem

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 "use strict";
-const items = Array.of("🍅", "🍄", "🥦", "🥕", "🥒", "🌽", "🥑");
+const items = ["🍅", "🍄", "🥦", "🥕", "🥒", "🌽", "🥑"];
 // const setItems = new Set(items);
 // const newItems = Array.from(setItems);
 
 // const chars = Array.from("arslonbek");
 // console.log("chars = ", chars);
-console.log(items);
 // console.log(newItems);
+items.splice(0, 0, "🍉", "🍇", "🍓");
+console.log(items);
