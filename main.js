f5db0157c726cabf8f1438061d98f24c8a339952
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-22 14:01:51 +0500

    lesson-63 finish-code

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 "use strict";
 const items = ["🍅", "🍄", "🥦", "🥕", "🥒", "🌽", "🥑"];
+
+function fix45(nums) {
+  let indexesOf4 = [];
+  let indexesOf5 = [];
+
+  nums.forEach(function (num, idx) {
+    if (num === 4) indexesOf4.push(idx);
+    if (num === 5) indexesOf5.push(idx);
+  });
+  console.log("indexesOf4 = ", indexesOf4);
+  console.log("indexesOf5 = ", indexesOf5);
+
+  indexesOf4.forEach(function (num, idx) {
+    if (nums[num + 1] !== 5) {
+      nums[0] = nums[num + 1];
+      nums[num + 1] = 5;
+    }
+  });
+  return nums;
+}
+console.log([5, 4, 5, 4, 1]);
+console.log(fix45([5, 4, 5, 4, 1]));
+
+/**
+ *
+ * fix45([5, 4, 5, 4, 1]) → 1,4,5,4,5
+ */
