376a269708c5f5abf33c0b274093fc8961b9fe28
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-26 05:40:33 +0500

    lesson-66 start-codes

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
 "use strict";
+
+// // function-inside-function
+// const cutPieces = function (fruit) {
+//   return fruit * 4;
+// };
+
+// const fruitProcessor = function (apples, oranges) {
+//   const applePieces = cutPieces(apples);
+//   const orangePieces = cutPieces(oranges);
+
+//   // prettier-ignore
+//   const juice = `Juice with ${applePieces} pieces
+//                  of apple and ${orangePieces} pieces
+//                  of orange.`;
+//   return juice;
+// };
+
+// HOF
+// const oneWord = function (str) {
+// };
+
+// const upperFirstWord = function (str) {
+// };
+
+// const arr = [2, 3, 4, 5];
+// Higher-order function
+// const transformer = function (str, fn = () => {}) {
+//   console.log(arguments.length);
+//   console.log(fn.name);
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+// transformer('JavaScript is the best!');
+
+// SIDE EFFECT
+
+// let a = 20;
+
+// function calc() {
+//   a = a + 1;
+//   return true;
+// }
+
+// calc();
