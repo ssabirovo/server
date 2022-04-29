ac3e0d61b23a667040dbdacc1ad60502e761917e
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-29 15:03:01 +0500

    lesson-70 start-codes

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
 "use strict";
-
-setTimeout(() => {
-  console.log("I am first");
-}, 2000);
-
-setTimeout(() => {
-  console.log("I am second");
-}, 1000);
-
-setTimeout(() => {
-  console.log("I am third");
-}, 1500);
