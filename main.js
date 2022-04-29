a2affaab2260e8e96ecb15a610e953ca3f0288da
arslonbekXX <arslonbek.lion067@gmail.com>
2022-04-29 15:00:52 +0500

    lesson-69 finish-codes

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+setTimeout(() => {
+  console.log("I am first");
+}, 2000);
+
+setTimeout(() => {
+  console.log("I am second");
+}, 1000);
+
+setTimeout(() => {
+  console.log("I am third");
+}, 1500);
