3964d28cb2c96c64c360fb2f776139c47554326b
arslonbekXX <arslonbek.lion067@gmail.com>
2022-05-13 11:11:12 +0500

    lesson-74 sudoku-hw-finish

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let list = [];
+let counter = 0;
+for (let i = 0; i < 9; i++) {
+  let innerList = [];
+  for (let j = 0; j < 9; j++) {
+    innerList[j] = counter++;
+  }
+  list[i] = innerList;
+}
+
+console.log(list);
