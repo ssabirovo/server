be92b493233ff3967e259b82b40b656271ff6787
arslonbekXX <arslonbek.lion067@gmail.com>
2022-05-17 17:54:47 +0500

    lesson-77 finish-codes

--- main.js
+++ main.js
@@ -1,4 +1,50 @@
 "use strict";
-const elements = document.querySelectorAll(".app, .test");
+// const box1 = document.querySelector(".box-1");
+// const box2 = document.querySelector(".box-2");
 
-elements.forEach((el, idx) => (el.innerHTML = `idx = ${idx}`));
+// box1.textContent = "this is Box-1";
+
+// v2 querySelectorAll
+
+// const elements = document.querySelectorAll(
+//   ".box-1, .box-2[href='hello world']"
+// );
+// console.log("boxes = ", boxes);
+// boxes.forEach((box) => {
+//   box.textContent = "Hello World";
+// });
+
+// for (let element of elements) {
+//   element.innerHTML = "Hello World";
+// }
+
+// const box1 = document.querySelector(".box-1");
+// const ul = document.querySelector("ul");
+
+// console.log(ul.firstElementChild);
+// console.log(ul.firstElementChild.parentNode.parentNode.parentNode.parentNode);
+
+// const decrement = document.getElementById("dec");
+// const increment = document.getElementById("inc");
+// const div = document.querySelector("div");
+// const body = document.body;
+// let counter = 0;
+
+// const counterDiv = `<button onclick="body.style.backgroundColor='red'">Click Me</button>`;
+// body.innerHTML = counterDiv;
+
+// // events
+// increment.onclick = () => {
+//   counterDiv.textContent = `counter : ${++counter}`;
+// };
+// decrement.onclick = () => {
+//   counterDiv.textContent = `counter : ${--counter}`;
+// };
+const div = document.querySelector("div");
+const btn = document.createElement("button");
+btn.textContent = "click me".toUpperCase();
+btn.id = "click me";
+
+console.log(div.childNodes);
+div.insertBefore(btn, div.children[0]);
+console.log(div.childNodes);
