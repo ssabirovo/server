12ab5812c2e14e01f89582d47137e64014a7b414
arslonbekXX <arslonbek.lion067@gmail.com>
2022-05-18 08:38:36 +0500

    lesson-78 start-codes

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 "use strict";
+// Create the new node to insert
+let newNode = document.createElement("span");
+newNode.textContent = "arslonbek";
+// Get a reference to the parent node
+let parentDiv = document.getElementById("childElement").parentNode;
+
+// Begin test case [ 1 ] : Existing childElement (all works correctly)
+let sp2 = document.getElementById("childElement");
+parentDiv.insertBefore(newNode, sp2);
+// End test case [ 1 ]
+
+// Begin test case [ 2 ] : childElement is of Type undefined
+// let sp2 = undefined; // Non-existent node of id "childElement"
+// parentDiv.insertBefore(newNode, sp2); // Implicit dynamic cast to type Node
+// End test case [ 2 ]
+
+// Begin test case [ 3 ] : childElement is of Type "undefined" ( string )
+// let sp2 = "undefined"; // Non-existent node of id "childElement"
+// parentDiv.insertBefore(newNode, sp2); // Generates "Type Error: Invalid Argument"
+// End test case [ 3 ]
