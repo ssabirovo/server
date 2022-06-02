fcdf7191d0fbce339e756505e28be3127f2ae239
arslonbekXX <arslonbek.lion067@gmail.com>
2022-06-02 13:57:42 +0500

    lesson-88 finish-codes

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const request = new XMLHttpRequest();
+request.open("get", "https://jsonplaceholder.typicode.com/users");
+
+request.send();
+
+request.addEventListener("load", function () {
+  console.log("this = ", this);
+});
+
+/**
+ * CRUD
+ *  C -> create (post)
+ *  R -> read (get)
+ *  U -> update (put, patch)
+ *  D -> delete (delete)
+ *
+ *
+ *
+ *
+ */
