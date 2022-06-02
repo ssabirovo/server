a2152b75cad5dfa053bb0fda7965f38c3e725e22
arslonbekXX <arslonbek.lion067@gmail.com>
2022-06-02 14:02:13 +0500

    lesson-89 start-codes

--- cat-promise/data.js
+++
@@ -1,8 +0,0 @@
-export const images = [
-  "./images/img-1.jpg",
-  "./images/img-2.jpg",
-  "./images/img-3.jpg",
-  "./images/img-4.jpg",
-  "https://images.pexels.com/photos/847393/pexels-photo-847393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-];
