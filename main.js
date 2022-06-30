e6965d7bc53948b311b820511dffc6f1868e28b2
arslonbekXX <arslonbek.lion067@gmail.com>
2022-06-30 11:49:00 +0500

    lesson-107 finish-codes

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-"use strict";
+function foo() {
+  let x = (y = 0);
+  x++;
+  y++;
+  return x;
+}
+
+console.log(window.y);
+
+console.log(foo(), typeof x, typeof y);
+
+console.log(typeof window.y);
