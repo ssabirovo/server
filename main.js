21d6fd13ef8d44a82c5ef346939cf267a0fc8e7c
arslonbekXX <arslonbek.lion067@gmail.com>
2022-07-06 12:17:28 +0500

    lesson-111 finish-codes

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 "use strict";
+
+// const a = 20; // new Number()
+// const b = 50; // new Number()
+// console.log(a.__proto__ === Number.prototype);
+// console.log(b.__proto__ === Number.prototype);
+// console.log(Number.__proto__ === Function.prototype);
+// console.dir(Function.__proto__);
+
+// function box() {}
+
+// console.dir(box.prototype);
+
+function Foo(name) {
+  this.name = name;
+}
+
+function App(name) {
+  this.foo = {};
+  this.bar = new Foo(name);
+}
+
+const app = new App("Box");
+
+// console.log(app.__proto__ === App.prototype);
+// console.log(Function.prototype.__proto__ === Object.prototype);
+// app.foo.__proto__ ===
+
+function test() {} // new Function
+const a = 20; // new Number()
+const b = true; // new Boolean()
+console.log(test.__proto__ === Function.prototype);
+console.log(a.__proto__ === Number.prototype);
+console.log(b.__proto__ === Boolean.prototype);
+console.log(Boolean.__proto__ === Function.prototype);
+
+const person = {}; // new Object()
+
+console.log(person.__proto__.__proto__);
