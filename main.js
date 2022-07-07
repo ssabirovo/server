40a2030801ca876d56ab8033b9aea592ca7b9138
arslonbekXX <arslonbek.lion067@gmail.com>
2022-07-07 12:00:51 +0500

    lesson-113 start-codes

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
 "use strict";
-const user = {
-  name: "Arslonbek",
-  getUsers() {
-    console.log("users getting...");
-  },
-};
-
-console.log(user);
-
-const admin = Object.create(user);
-console.log(admin);
-
-console.log(admin.__proto__ === user);
