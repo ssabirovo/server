b34043c2042319cfe97c5650b76f86db5a45b70b
arslonbekXX <arslonbek.lion067@gmail.com>
2022-07-18 13:24:49 +0500

    lesson-118 finish-codes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class App extends Component {
   render(root) {
     return (
       <main className='container py-2'>
-        <Counter count={0} color='success' />
+        <Counter count={20} color='success' />
         <Counter count={20} color='primary' />
         <Counter count={200} color='warning' />
       </main>
